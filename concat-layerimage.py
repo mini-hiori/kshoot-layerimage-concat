5ba9c0256bfaa6ab50f913c4d6dc53959e02c4ea
mini-hiori18 <phantombisco@gmail.com>
2020-12-04 02:54:42 +0900

    インデックス指定にする必要なかったので修正

--- concat-layerimage.py
+++ concat-layerimage.py
@@ -6,22 +6,21 @@ def image_concatter(target_path):
     # 指定ディレクトリの画像読み込み
     imagelist = glob.glob(r"{}\*.bmp".format(target_path))
     print(imagelist)
-    # 同一ディレクトリ画像リストをソート
+    # 読み込んだ画像リストをソート
     imagelist = sorted(imagelist, key=lambda x:int(x[-8:-4]))
     # 成果物画像一時保存用
     temp = None
-    for i in range(len(imagelist)):
-        if i == len(imagelist)-1:
-            break
+    for image_path in imagelist:
         # opencvで画像を1つずつ読み込み、tempの右端に足していく
         if temp is None:
-            temp = cv2.imread(imagelist[i])
+            temp = cv2.imread(image_path)
         else:
-            dst = cv2.imread(imagelist[i])
+            dst = cv2.imread(image_path)
             temp = cv2.hconcat([temp, dst])
     cv2.imwrite('{}\layer_concatted.jpg'.format(target_path), temp)
 
 if __name__ == '__main__':
     print("plz input target path")
+    # 連番bmpを配置したフォルダを入力する
     target_path = input()
     image_concatter(target_path)
